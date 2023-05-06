69842cfcb22a64e53d79cfda7a942f237d8213f6
fuzzymfx <anubhabr50@gmail.com>
2023-05-06 20:53:19 +0530

    proxy server init

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import bodyParser from "body-parser"; 
+import axios from "axios";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.raw({ type: 'text/plain' }))
+
+app.post('/post', async (req, res) => {
+	console.log(req.body)
+  try {
+    const response = await axios.post(process.env.API_URL, req.body, {
+      headers: {
+        'Content-Type': 'text/plain;charset=utf-8',
+        'Authorization': req.headers.authorization
+      }
+    });
+    res.send(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error');
+  }
+});
+
+app.listen(3000, () => {
+		console.log("Server is running on port 3000.");
+		console.log(process.env.API_URL	)
+	});
+
+
